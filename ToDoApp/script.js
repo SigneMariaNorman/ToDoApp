6deedfed455020142d19d3d41ce0888616af5922
Maria Norman <signemaria83@gmail.com>
2025-05-21 15:17:19 +0200

    Update script.js

--- ToDoApp/script.js
+++ ToDoApp/script.js
@@ -1,14 +1,23 @@
-let todos = []; // En tom array.
+let todos = []; // En tom array. "Todos" betyder ofta "Array"
+const buttonElem = document.querySelector('#add-todo-button'); //leta efter detta.
+console.log(buttonElem); 
 
-const buttonElem = document.querySelector('#add-todo-button'); 
-//Ovanstående ska söka efter add-todo-button.
-//console.log(document); //visar var den ligger.
-buttonElem.addEventListener('click', () => {
-    console.log('Nu klickade du på knappen!');
+function displayTodos() {
+for (let i = 0; i < 5; i++) {
+console.log('i är: ', i);
 }
-//ovanstående är ett event där man klickar på nånting.
+}
+buttonElem.addEventListener('click', function () {
+/*
+Måsvingarna ska vara inom parentesen, som ovan!
+console.log('Nu klickade du på knappen!');
+});
+ovanstående är ett event där man klickar på nånting 
+och det läggs till i arrayen, och sparas, element efter element.*/
 const todo = prompt('Vad vill du göra?');
 
 todos.push(todo);//pushar in 'Vad vill du göra' i den tomma arrayen
 
-console.log(todos);//Vad händer nu?
+console.log(todos);/*Funktionen körs allt som allt om gång på gång,
+pga functionen */
+});
